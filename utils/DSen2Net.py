b139bf9528f2fc9838726facc2781c1985ec3f88
Rodrigo Almeida <rodrigo.almeida@up42.com>
2020-08-19 17:01:58 +0200

    Add copied over AESR model

--- utils/DSen2Net.py
+++ utils/DSen2Net.py
@@ -2,6 +2,68 @@ from __future__ import division
 from keras.models import Model, Input
 from keras.layers import Conv2D, Concatenate, Activation, Lambda, Add
 
+class DenoisingAutoEncoderSR:
+
+    def __init__(self, scale_factor, action_type):
+        def __init__(self, model_name, scale_factor, action_type):
+        """
+        Base model to provide a standard interface of adding Super Resolution models
+        """
+        self.model = None  # type: Model
+        self.model_name = model_name
+        self.scale_factor = scale_factor
+        self.weight_path = None
+        self.action_type = action_type
+
+        self.type_scale_type = "norm"  # Default = "norm" = 1. / 255
+        self.type_requires_divisible_shape = False
+        self.type_true_upscaling = False
+
+        self.evaluation_func = None
+        self.uses_learning_phase = False
+
+
+        self.n1 = 64
+        self.n2 = 32
+
+        self.weight_path = "weights/Denoising_AutoEncoder_%dX.h5" % self.scale_factor
+        self.file_format = "tiff"
+
+    def create_model(self, height=32, width=32, channels=3, load_weights=False, batch_size=128):
+        """
+            Creates a model to remove / reduce noise from upscaled images.
+        """
+
+        # Perform check that model input shape is divisible by 4
+        if self.action_type == 'training':
+            height *= self.scale_factor
+            width *= self.scale_factor
+
+        if self.type_requires_divisible_shape and height is not None and width is not None:
+            assert height % 4 == 0, "Height of the image must be divisible by 4"
+            assert width % 4 == 0, "Width of the image must be divisible by 4"
+
+        if width is not None and height is not None:
+            shape = (width, height, channels)
+        else:
+            shape = (None, None, channels)
+
+        init = Input(shape=shape)
+
+        level1_1 = Conv2D(self.n1, (3, 3), activation='relu', padding='same')(init)
+        level2_1 = Conv2D(self.n1, (3, 3), activation='relu', padding='same')(level1_1)
+
+        level2_2 = Conv2DTranspose(self.n1, (3, 3), activation='relu', padding='same')(level2_1)
+        level2 = Add()([level2_1, level2_2])
+
+        level1_2 = Conv2DTranspose(self.n1, (3, 3), activation='relu', padding='same')(level2)
+        level1 = Add()([level1_1, level1_2])
+
+        decoded = Conv2D(channels, (5, 5), activation='linear', padding='same')(level1)
+
+        model = Model(init, decoded)
+        return model
+
 
 def resBlock(x, channels, kernel_size, scale=0.1):
     tmp = Conv2D(
